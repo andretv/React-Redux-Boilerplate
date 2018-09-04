4387289ed40cc02880f03e71ee0c8d9398ee542c
André Vaccari <andrevaccari95@gmail.com>
2018-09-04 20:40:15 -0300

    Api file simplifyed.

--- src/tools/api.js
+++ src/tools/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { store } from 'src/root/root';
+// import { store } from 'src/root/root';
 
 /**
  * Actions.
  */
-import { setToken } from 'modules/auth/actions/token';
+// import { setToken } from 'modules/auth/actions/token';
 
 /**
  * @constant BASE_URL comes from webpack DefinePlugin.
@@ -22,21 +22,21 @@ const instance = axios.create({
  * Intercept every api response checking if there's an authorization
  * token and sets it to the store.
  */
-instance.interceptors.response.use((response) => {
-  if (response.headers.authorization) {
-    store.dispatch(setToken(response.headers.authorization));
-  }
+// instance.interceptors.response.use((response) => {
+//   if (response.headers.authorization) {
+//     store.dispatch(setToken(response.headers.authorization));
+//   }
 
-  return response;
-});
+//   return response;
+// });
 
 /**
  * Intercept every store changes reassigning default authorization
  * header for api requests.
  */
-store.subscribe(() => {
-  const { auth: { token } } = store.getState();
-  Object.assign(instance.defaults, { headers: { authorization: token } });
-});
+// store.subscribe(() => {
+//   const { auth: { token } } = store.getState();
+//   Object.assign(instance.defaults, { headers: { authorization: token } });
+// });
 
 export default instance;
